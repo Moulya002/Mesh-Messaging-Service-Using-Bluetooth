08d62949f23d1693ba8eef33dee4da1d6b3eb32c
Moulya002 <77184468+Moulya002@users.noreply.github.com>
2025-11-15 14:46:42 -0600

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,21 @@
+import 'package:bluetooth_messenger/constants.dart';
+import 'package:bluetooth_messenger/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+void main() {
+WidgetsFlutterBinding.ensureInitialized();
+Firebase.initializeApp();
+runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+const MyApp({Key? key}) : super(key: key);
+@override
+Widget build(BuildContext context) {
+return MaterialApp(
+theme: CustomTheme.dark,
+title: 'Bluetooth Messenger',
+debugShowCheckedModeBanner: false,
+home: WelcomeScreen(context: context),
+);
+}
+}
